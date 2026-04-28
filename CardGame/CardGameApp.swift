868097da0c41a28dd4824b35c69b228fc206b7c9
EdwardPhan <EdwardPhan2015@gmail.com>
2026-04-28 11:39:43 +0700

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Vinh Phan on 28/4/26.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
